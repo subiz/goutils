1f71e71851a9da2f7f4a7b3b6cfe81ae6cc475f8
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-06-28 19:39:33 +0700

    expire key

--- cache/cache.go
+++ cache/cache.go
@@ -13,13 +13,31 @@ type Cache struct {
 	lc      *lru.Cache
 	loadf   func(key string) ([]byte, error)
 	prefix  string
+	expires map[string]time.Time
+	exp     time.Duration
+}
+
+func (c *Cache) clearExpire() {
+	for {
+		c.Lock()
+		for k, v := range c.expires {
+			if time.Since(v) < c.exp {
+				continue
+			}
+			c.lc.Remove(k)
+			delete(c.expires, k)
+		}
+		c.Unlock()
+		time.Sleep(5 * time.Minute)
+	}
 }
 
 func New(prefix string, localsize int, redishosts []string, redispassword string,
-	loadf func(string) ([]byte, error),
-	removef func(string) error) *Cache {
+	loadf func(string) ([]byte, error), expire time.Duration) *Cache {
 	c := &Cache{
 		Mutex:   &sync.Mutex{},
+		expires: make(map[string]time.Time),
+		exp:     expire,
 		loadf:   loadf,
 		rclient: &goredis.Client{},
 	}
@@ -29,20 +47,23 @@ func New(prefix string, localsize int, redishosts []string, redispassword string
 	if err != nil {
 		panic(err)
 	}
+	go c.clearExpire()
 	return c
 }
 
 func (c *Cache) Get(key string) ([]byte, error) {
 	c.Lock()
 	defer c.Unlock()
+
 	v, ok := c.lc.Get(key)
-	if ok {
+	if ok && time.Since(c.expires[key]) < c.exp {
 		return v.([]byte), nil
 	}
 
 	// local cache miss
-	byts, err := c.rclient.Get(c.prefix + key, c.prefix + key)
+	byts, err := c.rclient.Get(c.prefix+key, c.prefix+key)
 	if err == nil {
+		c.expires[key] = time.Now()
 		c.lc.Add(key, byts) // store back to client
 		return byts, nil
 	}
@@ -54,8 +75,9 @@ func (c *Cache) Get(key string) ([]byte, error) {
 	}
 
 	// store back
+	c.expires[key] = time.Now()
 	c.lc.Add(key, byts)
-	c.rclient.Set(c.prefix + key, c.prefix + key, byts, 10*time.Minute) // ignore err
+	c.rclient.Set(c.prefix+key, c.prefix+key, byts, 10*c.exp) // ignore err
 	return byts, nil
 }
 
@@ -63,12 +85,18 @@ func (c *Cache) Set(key string, value []byte) error {
 	c.Lock()
 	defer c.Unlock()
 	c.lc.Add(key, value)
-	return c.rclient.Set(c.prefix + key, c.prefix + key, value, 10*time.Minute)
+	c.expires[key] = time.Now()
+	return c.rclient.Set(c.prefix+key, c.prefix+key, value, 10*c.exp)
+}
+
+func (c *Cache) remove(key string) error {
+	c.lc.Remove(key)
+	delete(c.expires, key)
+	return c.rclient.Expire(c.prefix+key, c.prefix+key, 0)
 }
 
 func (c *Cache) Remove(key string) error {
 	c.Lock()
 	defer c.Unlock()
-	c.lc.Remove(key)
-	return c.rclient.Expire(c.prefix + key, c.prefix + key, 0)
+	return c.remove(key)
 }
