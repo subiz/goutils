104f9308e60ac6ce7f34a72e39bcc2519e14428e
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-07-10 01:01:25 +0700

    add conv

---
+++ conv/conv.go
@@ -0,0 +1,35 @@
+package conv
+
+import "fmt"
+
+var t__, f__ = true, false
+var True, False = &t__, &f__
+
+func S(s interface{}) *string {
+	switch v := s.(type) {
+	case string:
+		return &v
+	case fmt.Stringer:
+		str := v.String()
+		return &str
+	default:
+		str := fmt.Sprintf("%v", v)
+		return &str
+	}
+}
+
+func AmpInt32(i int32) *int32 { return &i }
+
+func AmpI64(i int64) *int64 { var ix = int64(i); return &ix }
+
+func B(b bool) *bool { return &b }
+
+func PI32(i int) *int32 {
+	i32 := int32(i)
+	return &i32
+}
+
+func PI64(i int) *int64 {
+	i64 := int64(i)
+	return &i64
+}
