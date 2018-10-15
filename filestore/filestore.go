213d7e40eec2894cbd708defa0fb1af5cbaa0d05
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-10-15 14:55:28 +0700

    add errors

--- filestore/filestore.go
+++ filestore/filestore.go
@@ -1,12 +1,12 @@
 package file
 
 import (
-	"context"
-	"io"
-	"os"
-
 	"cloud.google.com/go/storage"
+	"context"
+	"git.subiz.net/errors"
+	cpb "git.subiz.net/header/common"
 	"google.golang.org/api/option"
+	"io"
 )
 
 type FileStore interface {
@@ -22,19 +22,16 @@ type GS struct {
 func NewGS(bucketName, credFile string) *GS {
 	ctx := context.Background()
 
-	if credFile == "" && os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") != "" {
-		credFile = os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	if credFile == "" {
+		panic(errors.New(400, cpb.E_invalid_filestore_credential, "credFile cannot be empty"))
 	}
 
 	client, err := storage.NewClient(ctx, option.WithCredentialsFile(credFile))
 	if err != nil {
-		panic(err)
+		panic(errors.Wrap(err, 400, cpb.E_invalid_filestore_credential))
 	}
 
-	return &GS{
-		client:     client,
-		bucketName: bucketName,
-	}
+	return &GS{client: client, bucketName: bucketName}
 }
 
 func (s *GS) Add(id string, r io.Reader, contentType string) error {
@@ -47,11 +44,11 @@ func (s *GS) Add(id string, r io.Reader, contentType string) error {
 	}
 
 	if _, err := io.Copy(w, r); err != nil {
-		return err
+		return errors.Wrap(err, 500, cpb.E_filestore_write_error)
 	}
 
 	if err := w.Close(); err != nil {
-		return err
+		return errors.Wrap(err, 500, cpb.E_filestore_write_error)
 	}
 
 	return nil
@@ -63,7 +60,7 @@ func (s *GS) Get(id string) (io.Reader, error) {
 
 	r, err := obj.NewReader(ctx)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, 500, cpb.E_filestore_read_error)
 	}
 
 	return r, nil
@@ -73,7 +70,7 @@ func (s *GS) MakePublic(id string) error {
 	ctx := context.Background()
 	acl := s.client.Bucket(s.bucketName).Object(id).ACL()
 	if err := acl.Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
-		return err
+		return errors.Wrap(err, 500, cpb.E_filestore_acl_error)
 	}
 
 	return nil
@@ -83,7 +80,7 @@ func (s *GS) MakePrivate(id string) error {
 	ctx := context.Background()
 	acl := s.client.Bucket(s.bucketName).Object(id).ACL()
 	if err := acl.Delete(ctx, storage.AllUsers); err != nil {
-		return err
+		return errors.Wrap(err, 500, cpb.E_filestore_acl_error)
 	}
 
 	return nil
