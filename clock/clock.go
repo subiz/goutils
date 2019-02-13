e6bdfd82191689a96c0851fad1c93033917a25cb
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2019-02-13 22:38:37 +0700

    add SplitTzOffset

--- clock/clock.go
+++ clock/clock.go
@@ -129,18 +129,64 @@ func TimezoneToUTC(tzName string) string {
 
 // tz: 07:00
 func ConvertTimezone(t time.Time, tz string) (year, mon, day, hour, min int, weekday string, err error) {
-	tz = strings.TrimSpace(tz)
-	tzsplit := strings.Split(tz, ":")
-	if len(tzsplit) != 2 {
-		return 0, 0, 0, 0, 0, "", fmt.Errorf("invalid timezone %s", tz)
+	tzhour, tzmin, err := SplitTzOffset(tz)
+	if err != nil {
+		return 0, 0, 0, 0, 0, "", err
 	}
 
-	tzhour, _ := strconv.Atoi(tzsplit[0])
-	tzmin, _ := strconv.Atoi(tzsplit[1])
-	if tz[0] == '-' {
-		tzmin = -tzmin
-	}
-
-	t = t.UTC().Add(time.Hour*time.Duration(tzhour) + time.Minute*time.Duration(tzmin))
-	return t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Weekday().String(), nil
+	t = t.UTC().Add(time.Hour*time.Duration(tzhour) +
+		time.Minute*time.Duration(tzmin))
+	return t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(),
+		t.Weekday().String(), nil
 }
+
+// SplitTzOffset splits timezone offset (e.g: +07:00) to hour and minute
+// pair (e.g: 7, 0)
+// timezone offset must follow +hh:mm or -hh:mm, otherwise the function
+// will return an invalid timezone offset error
+// examples:
+//  SplitTzOffset("+07:00") => 7, 0
+//  SplitTzOffset("-07:30") => -7, -30
+//  SplitTzOffset("-00:30") => -7, -30
+func SplitTzOffset(offset string) (int, int, error) {
+	offset = strings.TrimSpace(offset)
+	if len(offset) != 6 {
+		return 0, 0, fmt.Errorf("invalid timezone offset %s", offset)
+	}
+
+	sign := 1
+	if offset[0] == '-' {
+		sign = -1
+	} else if offset[0] == '+' {
+	} else {
+		return 0, 0, fmt.Errorf("invalid timezone offset %s", offset)
+	}
+
+	if offset[3] != ':' {
+		return 0, 0, fmt.Errorf("invalid timezone offset %s", offset)
+	}
+
+	if !isNumeric(offset[1]) || !isNumeric(offset[2]) ||
+		!isNumeric(offset[4]) || !isNumeric(offset[5]) {
+		return 0, 0, fmt.Errorf("invalid timezone offset %s", offset)
+	}
+	hour := (offset[1]-'0')*10 + offset[2] - '0'
+	min := (offset[4]-'0')*10 + offset[5] - '0'
+
+	// why not 24?, see https://en.wikipedia.org/wiki/List_of_UTC_time_offsets
+	// for the list of all possible timezone
+	if hour > 14 {
+		return 0, 0, fmt.Errorf("invalid timezone offset %s", offset)
+	}
+
+	if min > 60 {
+		return 0, 0, fmt.Errorf("invalid timezone offset %s", offset)
+	}
+
+	if sign == -1 {
+		return -int(hour), -int(min), nil
+	}
+	return int(hour), int(min), nil
+}
+
+func isNumeric(r byte) bool { return '0' <= r && r <= '9' }
