4f16b54203d03f232e72218ea4c79f33efb6c74c
Thanh Pham Kieu <thanhpk@live.com>
2024-09-29 12:15:29 +0700

    add loop limit

--- loop/loop.go
+++ loop/loop.go
@@ -2,16 +2,8 @@ package loop
 
 import (
 	"fmt"
-	"runtime"
-	"strings"
 	"time"
-	"os"
 )
-
-type Error struct {
-	Description string
-	Stack       string
-}
 
 func Loop(f func()) {
 	for {
@@ -36,39 +28,13 @@ func Loop(f func()) {
 	}
 }
 
-func LoopErr(f func() error) {
-	Loop(func() {
+func LoopErr(f func() error, maxtime int) error {
+	i := 0
+	return Loop(func() {
+		i++
+		if i > maxTime
 		if err := f(); err != nil {
 			panic(err)
 		}
 	})
-}
-
-func getMinifiedStack() string {
-	stack := ""
-	for i := 3; i < 90; i++ {
-		_, fn, line, _ := runtime.Caller(i)
-		if fn == "" {
-			break
-		}
-		hl := false // highlight
-		if strings.Contains(fn, "bitbucket.org/subiz") {
-			hl = true
-		}
-		var split = strings.Split(fn, string(os.PathSeparator))
-		var n int
-
-		if len(split) >= 2 {
-			n = len(split) - 2
-		} else {
-			n = len(split)
-		}
-		fn = strings.Join(split[n:], string(os.PathSeparator))
-		if hl {
-			stack += fmt.Sprintf("\n→ %s:%d", fn, line)
-		} else {
-			stack += fmt.Sprintf("\n→ %s:%d", fn, line)
-		}
-	}
-	return stack
 }
